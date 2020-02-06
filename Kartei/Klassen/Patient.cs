0dd950727b91cb420641b08adf77e2156e2330e0
Florian Daubenthaler <floriandaubenthaler@gmail.com>
2020-02-06 10:14:58 +0100

    Klassen Bearbeitet

--- Kartei/Klassen/Patient.cs
+++ Kartei/Klassen/Patient.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Kartei.Klassen
 {
-    public class Patient
+    public class Patient : Kartei
     {
         DateTime _geboren;
         string _vorname;
@@ -16,6 +16,11 @@ namespace Kartei.Klassen
         public string Vorname { get => _vorname; set => _vorname = value; }
         public string Nachname { get => _nachname; set => _nachname = value; }
 
-        public int Alter { get => DateTime.Today.Year - _geboren.Year;}
+        public int Alter { get => DateTime.Today.Year - _geboren.Year; }
+
+        public Patient(int Alter, string _vorname, string _nachname, string _beschwerde, string _diagnose, DateTime _datum, int Krankheitsdauer, string _arzt, int _id)
+        {
+
+        }
     }
 }
